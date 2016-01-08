e3519537cac21d1b1e4b2dca8ca6358f168d2010
Hackbright Student <info@hackbrightacademy.com>
2016-01-07 16:31:17 -0800

    fixed inner_four_end() function

--- list_operations.py
+++ list_operations.py
@@ -137,6 +137,7 @@ def inner_four(input_list):
     """
 
     return input_list[2:6]
+    ## question: is there a method/function for getting the actual middle, generalizable?
 
 
 def inner_four_end(input_list):
@@ -151,7 +152,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
